a2e09e63df6a7bece6936086fabe9763294f372a
danislater <danielmolinaslater@gmail.com>
2024-06-13 12:39:42 +0200

    adding first time project

---
+++ app/Models/Recipe.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Recipe extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name',
+        'description',
+        'img1',
+        'img2',
+        'img3',
+        'img4',
+    ];
+}
